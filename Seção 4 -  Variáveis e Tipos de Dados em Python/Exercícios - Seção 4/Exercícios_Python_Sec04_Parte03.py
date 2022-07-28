368c0e9d485c6053e915f02242e3bb54ea5206bb
Rafael A.R da Rosa <rodrigues.rafaelantonio@gmail.com>
2022-07-27 21:23:07 -0300

    Lista parte 3 - Start

---
+++ Seção 4 -  Variáveis e Tipos de Dados em Python/Exercícios - Seção 4/Exercícios_Python_Sec04_Parte03.py
@@ -0,0 +1,43 @@
+"""
+
+
+22 - Leia um valor de comprimento em jardas e apresente-o convertido em metros. A fórmula
+de conversão é: M = 0,91 * J, sendo J o comprimento em jardas e M o comprimento
+em metros.
+
+23 - Leia um valor de comprimento em metros e apresente-o convertido em jardas. A fórmula
+de conversão é: J = M / 0,91, sendo J o comprimento em jardas e M o comprimento em
+metros.
+
+24 - Leia um valor de área em metros quadrados m² e apresente-o convertido em acres. A
+fórmula de conversão é: A = M * 0,000247, sendo M a área em metros quadrados e A
+a área em acres.
+
+25 - Leia um valor de área em acres e apresente-o convertido em metros quadrados m². A
+fórmula de conversão é: M = A * 4018,58 , sendo M a área em metros quadrados e A a
+área em acres.
+
+26 - Leia um valor de área em metros quadrados m² e apresente-o convertido em hectares.
+A fórmula de conversão é: H = M * 0,0001, sendo M a área em metros quadrados e H
+a área em hectares.
+
+27 - Leia um valor de área em hectares e apresente-o convertido em metros quadrados m
+A fórmula de conversão é: M = H * 10000, sendo M a área em metros quadrados e H
+aárea em hectares.
+
+28 - Faça a leitura de três valores e apresente como resultado a soma dos quadrados dos
+três valores lidos.
+
+29 - Leia quatro notas, calcule a média aritmética e imprima o resultado.
+
+30 - Leia um valor em real e a cotação do dólar. Em seguida, imprima o valor correspondente
+em dólares.
+"""
+
+print('21 - Leia um valor de massa em libras e apresente-o convertido em quilogramas. A fórmula de conversão é: K = L * 0 45, sendo K a massa em quilogramas e L a massa em libras.')
+
+l1 = float(input('Por gentileza, informe o valor em libras para saber o valor e quilos: '))
+
+k1 = l1 * 0.45
+
+print(f'{l1} Libras é igual a {k1:.2f} quilos')
