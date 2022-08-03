d723fc4393e51fae0579da789573c28b08f294f3
Rafael A.R da Rosa <rodrigues.rafaelantonio@gmail.com>
2022-08-02 21:28:26 -0300

    Lista parte 3 - Start

--- Seção 4 -  Variáveis e Tipos de Dados em Python/Exercícios - Seção 4/Exercícios_Python_Sec04_Parte03.py
+++ Seção 4 -  Variáveis e Tipos de Dados em Python/Exercícios - Seção 4/Exercícios_Python_Sec04_Parte03.py
@@ -1,18 +1,6 @@
 """
 
 
-22 - Leia um valor de comprimento em jardas e apresente-o convertido em metros. A fórmula
-de conversão é: M = 0,91 * J, sendo J o comprimento em jardas e M o comprimento
-em metros.
-
-23 - Leia um valor de comprimento em metros e apresente-o convertido em jardas. A fórmula
-de conversão é: J = M / 0,91, sendo J o comprimento em jardas e M o comprimento em
-metros.
-
-24 - Leia um valor de área em metros quadrados m² e apresente-o convertido em acres. A
-fórmula de conversão é: A = M * 0,000247, sendo M a área em metros quadrados e A
-a área em acres.
-
 25 - Leia um valor de área em acres e apresente-o convertido em metros quadrados m². A
 fórmula de conversão é: M = A * 4018,58 , sendo M a área em metros quadrados e A a
 área em acres.
@@ -33,7 +21,7 @@ três valores lidos.
 30 - Leia um valor em real e a cotação do dólar. Em seguida, imprima o valor correspondente
 em dólares.
 """
-
+"""
 print('21 - Leia um valor de massa em libras e apresente-o convertido em quilogramas. A fórmula de conversão é: K = L * 0 45, sendo K a massa em quilogramas e L a massa em libras.')
 
 l1 = float(input('Por gentileza, informe o valor em libras para saber o valor e quilos: '))
@@ -41,3 +29,25 @@ l1 = float(input('Por gentileza, informe o valor em libras para saber o valor e 
 k1 = l1 * 0.45
 
 print(f'{l1} Libras é igual a {k1:.2f} quilos')
+"""
+"""
+print(
+    '22 - Leia um valor de comprimento em jardas e apresente-o convertido em metros. A fórmula de conversão é: M = 0,91 * J, sendo J o comprimento em jardas e M o comprimento em metros.')
+
+j1 = float(input('informe o valor em jardas para verificar seu valor em metros: '))
+
+m1 = 0.91 * j1
+
+print(f'{j1} Jardas é igual a {m1:.2f} metros')
+"""
+"""
+print('23 - Leia um valor de comprimento em metros e apresente-o convertido em jardas. A fórmula de conversão é: J = M / 0,91, sendo J o comprimento em jardas e M o comprimento em metros.')
+
+m2 = float(input('informe o valor em metros para verificar seu valor em jardas: '))
+
+j2 = m2 / 0.91
+
+print(f'{m2} Metros é igual a {j2:.2f} Jardas')
+"""
+
+print('24 - Leia um valor de área em metros quadrados m² e apresente-o convertido em acres. A fórmula de conversão é: A = M * 0,000247, sendo M a área em metros quadrados e A a área em acres.')
