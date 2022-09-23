c13f2764bd5226aef8e596e8f1b8b17869148a13
Rafael A.R da Rosa <rodrigues.rafaelantonio@gmail.com>
2022-09-22 23:24:18 -0300

    Lista parte 6 - tarefa 52 Lista parte 5 - Ajuste

--- Seção 4 -  Variáveis e Tipos de Dados em Python/Exercícios - Seção 4/Exercícios_Python_Sec04_Parte05.py
+++ Seção 4 -  Variáveis e Tipos de Dados em Python/Exercícios - Seção 4/Exercícios_Python_Sec04_Parte05.py
@@ -1,5 +1,3 @@
-
-"""
 print('41 - Faça um programa que leia o valor da hora de trabalho (em reais) e número de horas.'
       'trabalhadas no mês. Imprima o valor a ser pago ao funcionário, adicionando 10% sobre'
       'o valor calculado.')
@@ -10,10 +8,8 @@ valorPago = valorHora * nHorasTrabalho
 vAdicional = valorPago + (valorPago * 0.10)
 
 print(f'O valor a ser pago é de R${valorPago:.2f}, com o adicional de 10%, o valor será {vAdicional:.2f}')
-"""
-import datetime
 
-"""
+
 print('42 - Receba o salário-base de um funcionário. Calcule e imprima o salário a receber, sabendo-'
       'se que esse funcionário tem uma gratificação de 5% sobre o salário-base. Além disso,'
       'ele paga 7% de imposto sobre o salário-base.')
@@ -27,8 +23,7 @@ print(f' - Salário base do funcionário é de R${salarioBase:.2f}\n'
       f' - Gratificação do funcionário é de R${salarioGrati:.2f}\n'
       f' - Imposto sobre o salário Base é de R${impostoSalario:.2f}\n'
       f' - Salario Líquido é de R$ {salarioLiquido:.2f}')
-"""
-"""
+
 print('43 - Escreva um programa de ajuda para vendedores. A partir de um valor total lido, mostre:'
       '• O total a pagar com desconto de 10%;'
       '• O valor de cada parcela, no parcelamento de 3x sem juros;'
@@ -45,8 +40,7 @@ print(f'O total a pagar com desconto de 10%: R${vDescont10:.2f}\n'
       f'O valor de cada parcela, no parcelamento de 3x sem juros: R${vParcela3:.2f}\n'
       f'A comissão do vendedor, no caso da venda ser a vista (5% sobre o valor com desconto): R${comissaoVista}\n'
       f'A comissão do vendedor, no caso da venda ser parcelada (5% sobre o valor total): R${comissaoParcela:.2f}')
-"""
-"""
+
 print('44 - Receba a altura do degrau de uma escada e a altura que o usuário deseja alcançar'
       'subindo a escada. Calcule e mostre quantos degraus o usuário deverá subir para atingir'
       'seu objetivo.')
@@ -56,15 +50,13 @@ alturaAlcancar = float(input('Informe a altura a qual deseja alcançar em metros
 quantidadeDegraus = alturaAlcancar / (alturaDegrau/1000)
 print('Aqui usaremos a NR-31 - definição de 200mm de espaço entre degraus ')
 print(f'Para atingir a altura de {alturaAlcancar:.2f}m, é necessario subir {quantidadeDegraus:.2f}')
-"""
-"""
+
 print('45 - Faça um programa para converter uma letra maiúscula em letra minúscula. Use a tabela'
       'ASCII para resolver o problema.')
 
 letra = str(input('Informe uma letra maiúscula para ser convertida em minuscula: '))
 print(f'Convertendo a letra {letra} fica {letra.lower()}')
-"""
-"""
+
 print('46 -Faça um programa que leia um número inteiro positivo de três dígitos (de 100 a 999).'
       'Gere outro número formado pelos dígitos invertidos do número lido. Exemplo:'
       'limeroLido = 123'
@@ -73,22 +65,21 @@ print('46 -Faça um programa que leia um número inteiro positivo de três dígi
 numeral3Digitos = int(input('Informe um número para ser invertido: '))
 numeral3Digitos = str(numeral3Digitos)
 print(f'O valor invertido é: {numeral3Digitos[::-1]}')
-"""
-"""
+
 print('47 - Leia um número inteiro de 4 dígitos (de 1000 a 9999) e imprima 1 dígito por linha.')
 numeral4Digitos = int(input('Informe um número interio de 4 digitos: '))
 numeral4Digitos = str(numeral4Digitos)
 
 print(f'{numeral4Digitos[0]}\n{numeral4Digitos[1]}\n{numeral4Digitos[2]}\n{numeral4Digitos[3]}\n')
-"""
-"""
+
+
 print('48 - Leia um valor inteiro em segundos, e imprima-o em horas, minutos e segundos.')
 segundos = float(input('Informe o valor interio em segundos para saber horas,minutos e segundos: '))
 minutos = segundos / 60
 horas = minutos / 60
 
 print(f'{segundos} segundos, equivalem a {minutos:.2f} minutos, e a {horas:.2f} horas')
-"""
+
 
 print('50 - Implemente um programa que calcule o ano de nascimento de uma pessoa apartir de sua idade e do ano atual')
 ano =  2022
--- Seção 4 -  Variáveis e Tipos de Dados em Python/Exercícios - Seção 4/Exercícios_Python_Sec04_Parte06.py
+++ Seção 4 -  Variáveis e Tipos de Dados em Python/Exercícios - Seção 4/Exercícios_Python_Sec04_Parte06.py
@@ -1,5 +1,5 @@
 import math
-
+"""
 print('51 - Escreva um programa que leia as coordenadas x e y de pontos no R² e calcule sua distância da origem (0,0).')
 eixoX = int(input('Informe a coordenada do eixo X: '))
 eixoY = int(input('Informe as coodenadas do eixo Y: '))
@@ -7,13 +7,25 @@ eixoY = int(input('Informe as coodenadas do eixo Y: '))
 distancia = math.sqrt((math.pow(eixoX,2)+(math.pow(eixoY,2))))
 
 print(f'A distância entre a origem e as coordenadas é igual a {distancia:.2f}')
+"""
 
-"""
-52 - Três amigos jogaram na loteria. Caso eles ganhem, o prêmio deve ser repartido pro-
-porcionalmente ao valor que cada deu para a realização da aposta. Faça um programa
-que leia quanto cada apostador investiu, o valor do prêmio, e imprima quanto cada um
-ganharia do prêmio com base no valor investido.
-"""
+print('52 - Três amigos jogaram na loteria. Caso eles ganhem, o prêmio deve ser repartido pro-'
+      'porcionalmente ao valor que cada deu para a realização da aposta. Faça um programa'
+      'que leia quanto cada apostador investiu, o valor do prêmio, e imprima quanto cada um'
+      'ganharia do prêmio com base no valor investido.')
+
+valorPremio = float(input('Informe o valor do prêmio:R$ '))
+invest01 = float(input('Informe a porcentagem que o apostador 01 investiu: '))
+invest02 = float(input('Informe a porcentagem que o apostador 02 investiu: '))
+invest03 = 100 - (invest01 + invest02)
+print(f'Consequentemente, o apostador 03 investiu {invest03}%')
+
+premio01 = valorPremio * (invest01/100)
+premio02 = valorPremio * (invest02/100)
+premio03 = valorPremio * (invest03/100)
+
+print(f'Cada um recenerá {premio01:.2f}, {premio02:.2f} e {premio03:.2f} respectivamente ')
+
 """
 53 - Faça um programa para ler as dimensões de um terreno (comprimento c e largura l),
 bem como o preço do metro de tela p. Imprima o custo para cercar este mesmo terreno
