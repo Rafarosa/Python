45a3df2b1a686a808a8ac13b3f7252c2092c1e27
Rafael A.R da Rosa <rodrigues.rafaelantonio@gmail.com>
2022-09-29 21:12:19 -0300

    Lista parte 1 - Tarefa 1

---
+++ Seção5 -  Estruturas Lógicas e Condicionais em Python/Exercícios - Seção 5/Exercícios_Python_Sec05_Parte01.py
@@ -0,0 +1,57 @@
+import math
+
+print("1- Faça um programa que receba dois números e mostre qual deles é o maior.")
+
+numero01 = float(input('Informe um número: '))
+numero02 = float(input('Informe outro número: '))
+
+if numero01 > numero02:
+    print(f'{numero01} é mairo que {numero02}')
+else:
+    print(f'{numero02} é mairo que {numero02}')
+
+"""
+2- Leia um número fornecido pelo usuário. Se esse número for positivo, calcule a raiz
+quadrada do número. Se o número for negativo, mostre uma mensagem dizendo que o
+número é inválido.
+"""
+"""
+3- Leia um numero real. Se o número for positivo imprima a raiz quadrada. Do contrário,
+imprima o numero ao quadrado.
+"""
+"""
+4- Faça um programa que leia um número e, caso ele seja positivo, calcule e mostre:
+
+ - O número digitado ao quadrado
+ - A raiz quadrada do número digitado
+"""
+"""
+5- Faça um programa que receba um número inteiro e verifique se este número é par ou
+impar.
+"""
+"""
+6- Escreva um programa que, dados dois números inteiros, mostre na tela o maior deles,
+assim como a diferença existente entre ambos.
+"""
+"""
+7- Faça um programa que receba dois números e mostre o maior. Se por acaso, os dois
+números forem iguais, imprima a mensagem Números iguais.
+"""
+"""
+8- Faça um programa que leia 2 notas de um aluno, verifique se as notas são válidas e
+exiba na tela a média destas notas. Uma nota válida deve ser, obrigatoriamente, um
+valor entre 0.0 e 10.0, onde caso a nota não possua um valor válido, este fato deve ser
+informado ao usuário e o programa termina.
+"""
+"""
+9- Leia o salário de um trabalhador e o valor da prestação de um empréstimo. Se a
+prestação for maior que 20% do salário imprima: Empréstimo não concedido, caso
+contrário imprima: Empréstimo concedido.
+"""
+"""
+10- Faça um programa que receba a altura e o sexo de uma pessoa e calcule e mostre seu
+peso ideal, utilizando as seguintes fórmulas (onde h corresponde à altura):
+
+ Homens: (72.7 * h) - 58
+ Mulheres: (62.1 * h) -  44.7
+"""
