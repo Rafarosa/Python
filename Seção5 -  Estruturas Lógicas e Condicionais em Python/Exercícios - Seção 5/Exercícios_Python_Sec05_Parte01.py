9423f1635d3c5d1049cddede0366efc87586c948
Rafael A.R da Rosa <rodrigues.rafaelantonio@gmail.com>
2022-10-02 17:53:34 -0300

    Lista parte 1 - Tarefa 10 (Revisão) - final

--- Seção5 -  Estruturas Lógicas e Condicionais em Python/Exercícios - Seção 5/Exercícios_Python_Sec05_Parte01.py
+++ Seção5 -  Estruturas Lógicas e Condicionais em Python/Exercícios - Seção 5/Exercícios_Python_Sec05_Parte01.py
@@ -1,5 +1,5 @@
 import math
-"""
+
 print("1- Faça um programa que receba dois números e mostre qual deles é o maior.")
 
 numero01 = float(input('Informe um número: '))
@@ -9,8 +9,7 @@ if numero01 > numero02:
     print(f'{numero01} é mairo que {numero02}')
 else:
     print(f'{numero02} é mairo que {numero02}')
-"""
-"""
+
 print("2- Leia um número fornecido pelo usuário. Se esse número for positivo, calcule a raiz"
       "quadrada do número. Se o número for negativo, mostre uma mensagem dizendo que o"
       "número é inválido.")
@@ -21,8 +20,7 @@ if numex02 > 0:
     print(f'O númro informado é {numex02} e sua raiz quadrada é {math.sqrt(numex02):.2f}')
 else:
     print('Número inválido')
-"""
-"""
+
 print('3- Leia um numero real. Se o número for positivo imprima a raiz quadrada. Do contrário,'
       'imprima o numero ao quadrado.')
 
@@ -32,8 +30,7 @@ if numex03 > 0:
     print(f'O númro informado é {numex03} e sua raiz quadrada é {math.sqrt(numex03):.2f}')
 else:
     print(f'O quadrado {numex03} é {math.pow(numex03,2)}')
-"""
-"""
+
 print('4- Faça um programa que leia um número e, caso ele seja positivo, calcule e mostre:'
       '- O número digitado ao quadrado'
       '- A raiz quadrada do número digitado ')
@@ -45,8 +42,7 @@ if numex04 > 0:
     print(f'A raiz quadrada do número {numex04} é {math.sqrt(numex04):.2f}')
 else:
     print('Número fora do escopo')
-"""
-"""
+
 print('5- Faça um programa que receba um número inteiro e verifique se este número é par ou impar.')
 
 numex05 = int(input('Informe um número: '))
@@ -55,8 +51,7 @@ if (numex05 % 2) >= 1:
     print(f'número {numex05} é impar')
 else:
     print(f'número {numex05} é par ')
-"""
-"""
+
 print('6- Escreva um programa que, dados dois números inteiros, mostre na tela o maior deles,'
       'assim como a diferença existente entre ambos.')
 
@@ -67,8 +62,7 @@ if num_a_ex06 > num_b_ex06:
     print(f'{num_a_ex06} é mairo que {num_b_ex06}, a diferença entre eles é de {num_a_ex06 - num_b_ex06} ')
 else:
     print(f'{num_b_ex06} é mairo que {num_a_ex06}, a diferença entre eles é de {num_b_ex06 - num_a_ex06} ')
-"""
-"""
+
 print('7- Faça um programa que receba dois números e mostre o maior. Se por acaso, os dois'
       'números forem iguais, imprima a mensagem Números iguais.')
 
@@ -81,8 +75,7 @@ elif num_c_ex07 < num_d_ex07:
     print(f'{num_d_ex07} é mairo que {num_c_ex07}')
 else:
       print('Os números são iguais!!')
-"""
-"""
+
 print('8- Faça um programa que leia 2 notas de um aluno, verifique se as notas são válidas e'
       'exiba na tela a média destas notas. Uma nota válida deve ser, obrigatoriamente, um'
       'valor entre 0.0 e 10.0, onde caso a nota não possua um valor válido, este fato deve ser'
@@ -95,8 +88,7 @@ if (nota1Ex08 > 0 and nota1Ex08 <=10) and (nota2Ex08 >0 and nota2Ex08 <=10):
       print(f'a média das notas é {(nota1Ex08 + nota2Ex08) / 2}')
 else:
       print('Uma das notas não é válida')
-"""
-"""
+
 print('9- Leia o salário de um trabalhador e o valor da prestação de um empréstimo. Se a'
       'prestação for maior que 20% do salário imprima: Empréstimo não concedido, caso'
       'contrário imprima: Empréstimo concedido.')
@@ -112,13 +104,13 @@ if prestacao <= percentual:
 else:
       print('Empréstimo não concedido!!')
 
-"""
 print('10- Faça um programa que receba a altura e o sexo de uma pessoa e calcule e mostre seu'
       'peso ideal, utilizando as seguintes fórmulas (onde h corresponde à altura):'
       ' Homens: (72.7 * h) - 58'
       ' Mulheres: (62.1 * h) -  44.7')
 
 sexo = str(input('Informe o seu sexo (M ou F): '))
+sexo= sexo.upper()
 if sexo == 'M' or sexo == 'F':
       h = float(input('Informe sua altura: '))
       if sexo == 'F':
