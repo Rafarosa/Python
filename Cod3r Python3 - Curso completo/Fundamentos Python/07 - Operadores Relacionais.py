549ae7081cef188d59d6ea327e0911fd2cfed1a3
Rafael A.R da Rosa <rodrigues.rafaelantonio@gmail.com>
2023-06-04 17:17:59 -0300

    Operadores relacionais e de atribuicão

---
+++ Cod3r Python3 - Curso completo/Fundamentos Python/07 - Operadores Relacionais.py
@@ -0,0 +1,9 @@
+# Operadores relacionais
+
+print(3 > 4)
+print(4 >= 3)
+print(1 < 2)
+print(1 <= 2)
+print(3 != 1)
+print(3 == 3)
+print(3 == '3')
---
+++ Cod3r Python3 - Curso completo/Fundamentos Python/08 - Operadores de Atribuicao.py
@@ -0,0 +1,28 @@
+# operadores de Atribuicão
+
+a = 3
+print(a)
+
+a = a + 7
+print(a)
+
+a += 5
+print(a)
+
+a -= 3
+print(a)
+
+a *= 2
+print(a)
+
+a /= 4
+print(a)
+
+a %= 4
+print(a)
+
+a **= 8
+print(a)
+
+a //= 256
+print(a)
