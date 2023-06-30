16995c5fd69165c4703dc483522649292b036110
Rafael A.R da Rosa <rodrigues.rafaelantonio@gmail.com>
2023-06-29 22:52:20 -0300

    modified:   "Udemy - Programa\303\247\303\243o em Python do b\303\241sico ao avan\303\247ado/Se\303\247\303\243o 5 - Estruturas L\303\263gicas e Condicionais em Python/22 - AND-OR-NOT-IS.py" 	new file:   "Udemy - Programa\303\247\303\243o em Python do b\303\241sico ao avan\303\247ado/Se\303\247\303\243o 5 - Estruturas L\303\263gicas e Condicionais em Python/exercicios-python-secao05-41e_v2.pdf"

---
+++ Udemy - Programação em Python do básico ao avançado/Seção 5 - Estruturas Lógicas e Condicionais em Python/21 - if else elif.py
@@ -0,0 +1,12 @@
+"""
+Estrutura concicional
+if, else, elif
+"""
+idade = 10
+
+if idade <18:
+    print('idade inferior a 18 anos')
+elif idade == 18:
+    print('Tem 18 anos')
+else:
+    print('Maior de idade')
---
+++ Udemy - Programação em Python do básico ao avançado/Seção 5 - Estruturas Lógicas e Condicionais em Python/22 - AND-OR-NOT-IS.py
@@ -0,0 +1,22 @@
+"""
+Estrutura lógica
+and(e)
+
+or(ou)
+
+not(não)
+
+is (é)
+
+Operador unário
+-not, is
+Operador binário
+- and, or
+"""
+ativo = False
+logado = False
+
+if ativo and logado:
+    print('Bem vindo usuário')
+else:
+    print('Você tem que ativar a conta, acesse seu e-mail')
